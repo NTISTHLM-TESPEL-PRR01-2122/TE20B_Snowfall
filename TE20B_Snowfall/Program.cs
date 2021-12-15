0d2b7ea81cb3adf3dd2c28c9d56dbd46669d679c
Mikael Bergström <krank23@gmail.com>
2021-12-15 11:05:36 +0100

    Mer jul

--- TE20B_Snowfall/Program.cs
+++ TE20B_Snowfall/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Raylib_cs;
 
 Raylib.InitWindow(800, 600, "SnowFall");
@@ -6,22 +7,38 @@ Raylib.SetTargetFPS(60);
 Color skyBlue = new Color(135, 206, 235, 255);
 
 List<Rectangle> snowflakes = new List<Rectangle>();
+List<float> snowSpeed = new List<float>();
+Random generator = new Random();
 
-int s = 0;
-while (s < 7)
+for (int i = 0; i < 2000; i++)
 {
-  snowflakes.Add(new Rectangle(10 + s * 20, 10, 5, 5));
-  s++;
+  int x = generator.Next(Raylib.GetScreenWidth());
+  int y = generator.Next(Raylib.GetScreenHeight());
+  int size = generator.Next(2, 7);
+  snowflakes.Add(new Rectangle(x, y, size, size));
+
+  float speed = (float) (generator.NextDouble() + 0.5);
+  snowSpeed.Add(speed);
 }
+
 while (!Raylib.WindowShouldClose())
 {
   Raylib.BeginDrawing();
   Raylib.ClearBackground(skyBlue);
-  int i = 0;
-  while (i < 7)
+
+  for (int i = 0; i < snowflakes.Count; i++)
   {
+    Rectangle flake = snowflakes[i];
+
+    flake.y += snowSpeed[i];
+
+    if (flake.y > Raylib.GetScreenHeight())
+    {
+      flake.y = -10;
+    }
+    snowflakes[i] = flake;
+
     Raylib.DrawRectangleRec(snowflakes[i], Color.WHITE);
-    i++;
   }
 
   Raylib.EndDrawing();
