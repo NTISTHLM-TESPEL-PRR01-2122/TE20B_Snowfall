96bbe4aef03753a9a152fd41144092db4c6437c7
Mikael Bergström <krank23@gmail.com>
2021-12-15 10:35:50 +0100

    Initial commit

---
+++ TE20B_Snowfall/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "SnowFall");
+Raylib.SetTargetFPS(60);
+Color skyBlue = new Color(135, 206, 235, 255);
+
+List<Rectangle> snowflakes = new List<Rectangle>();
+
+int s = 0;
+while (s < 7)
+{
+  snowflakes.Add(new Rectangle(10 + s * 20, 10, 5, 5));
+  s++;
+}
+while (!Raylib.WindowShouldClose())
+{
+  Raylib.BeginDrawing();
+  Raylib.ClearBackground(skyBlue);
+  int i = 0;
+  while (i < 7)
+  {
+    Raylib.DrawRectangleRec(snowflakes[i], Color.WHITE);
+    i++;
+  }
+
+  Raylib.EndDrawing();
+}
